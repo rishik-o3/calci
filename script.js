0e4c99b63ea415157058595548c9eb75a5bcae90
rishik-o3 <110049046+rishik-o3@users.noreply.github.com>
2023-05-06 18:48:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function sub() {
     const diff = num1 - num2;
     document.getElementById("output").innerHTML = `The difference is: ${diff}`;
 }
+
+function mul() {
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+    const pro = num1 * num2;
+    document.getElementById("output").innerHTML = `The product is: ${pro}`;
+}
+
+function div() {
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+    const res = num1 / num2;
+    document.getElementById("output").innerHTML = `The answer is: ${res}`;
+}
